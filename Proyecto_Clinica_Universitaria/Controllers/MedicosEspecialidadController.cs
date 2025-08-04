9373a52f317bb0432bd4c9a91f78aa85f06564e1
crisce-03 <cristophercerritos0@gmail.com>
2025-08-04 15:44:02 -0600

    primer commit

---
+++ Proyecto_Clinica_Universitaria/Controllers/MedicosEspecialidadController.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+using Proyecto_Clinica_Universitaria.Models;
+using Proyecto_Clinica_Universitaria.Datos;
+
+namespace Proyecto_Clinica_Universitaria.Controllers
+{
+    public class MedicosEspecialidadController : Controller
+    {
+        MedicosEspecialidadDatos _MedicoEspecialidad = new MedicosEspecialidadDatos();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Guardar(MedicosEspecialidadModel especialidadmedico)
+        {
+            var respuesta = _MedicoEspecialidad.Guardar(especialidadmedico);
+
+            if (respuesta)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Proyecto_Clinica_Universitaria/Datos/MedicosEspecialidadDatos.cs
@@ -0,0 +1,44 @@
+﻿using Proyecto_Clinica_Universitaria.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Proyecto_Clinica_Universitaria.Datos
+{
+    public class MedicosEspecialidadDatos
+    {
+
+        public bool Guardar(MedicosEspecialidadModel especialidadmedico)
+        {
+            bool result;
+
+            try
+            {
+                var cn = new Conexion();
+
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+                {
+                    conexion.Open();
+
+                    SqlCommand cmd = new SqlCommand("sp_GuardarMedicosEspecialidad", conexion);
+                    cmd.Parameters.AddWithValue("@Especialidad", especialidadmedico.Especialidad ?? (object)DBNull.Value);
+                    cmd.Parameters.AddWithValue("@Descripcion", especialidadmedico.Descripcion ?? (object)DBNull.Value);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                result = true;
+            }
+            catch (Exception ex)
+            {
+
+                result = false;
+            }
+
+            return result;
+        }
+
+        
+
+    }
+}
